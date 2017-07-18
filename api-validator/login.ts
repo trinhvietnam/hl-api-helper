fc00d76416645985756305de4abe665166f95a2f
Trinh Viet Nam <trinhvietnambk@gmail.com>
2017-07-18 13:43:42 +0700

    add validator for login api

--- api-validator/login.ts
+++ api-validator/login.ts
@@ -6,6 +6,9 @@ export const VALIDATION_LOGIN ={
     options: { allowUnknownBody: false },
     body: {
         email: Joi.string().email().required(),
-        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
+        password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+		platform: Joi.string().required(),
+        os: Joi.string().required(),
+        pushToken: Joi.string().when('platform', { is: PLATFORM.MOBILE, then: Joi.required() }),
     }
 }
