388b087ac7096e59d29d8c2f264460e1b37a23c5
DESKTOP-8CA0JAG\nam <trinhvietnambk@gmail.com>
2017-07-22 16:38:53 +0700

    add response login

---
+++ response-sample/login.ts
@@ -0,0 +1,110 @@
+var responseLoginOK = {
+    "error": null,
+    "data": [
+        {
+            "id": 910200869,
+            "accountStatus": null,
+            "accountType": null,
+            "address": null,
+            "avatar": null,
+            "city": "Hà Nội",
+            "country": "Việt Nam",
+            "createdById": null,
+            "district": null,
+            "email": "trinhvietnambk@gmail.com",
+            "emailNotifies": null,
+            "entityName": "account",
+            "entityStatus": "1",
+            "fax": null,
+            "isValidatePhone": null,
+            "isWaitingResetPassword": null,
+            "language": null,
+            "lastName": null,
+            "location": null,
+            "modifiedById": "910200869",
+            "name": "Trịnh Viết Nam",
+            "numOfFollower": null,
+            "numOfFollowing": "2",
+            "numOfRating": null,
+            "numOfRating1Star": null,
+            "numOfRating2Star": null,
+            "numOfRating3Star": null,
+            "numOfRating4Star": null,
+            "numOfRating5Star": null,
+            "numOfView": null,
+            "parentId": null,
+            "parentType": null,
+            "paymentMethods": null,
+            "phone": null,
+            "suggestEmail": "trinhvietnambk",
+            "taxCode": null,
+            "tempPassword": null,
+            "timeCreate": "2017-04-27T17:27:02.330Z",
+            "timeModify": "2017-07-21T15:28:11.767Z",
+            "type": null,
+            "verifies": null,
+            "website": null,
+            "keyJWT": 1500658808876,
+            "mapRoles": {
+                "910200869": {
+                    "roleFor": "account",
+                    "userId": "910200869",
+                    "entityId": "910200869",
+                    "createdById": null,
+                    "entityName": "role",
+                    "entityStatus": "1",
+                    "id": "1105731116",
+                    "modifiedById": null,
+                    "parentId": null,
+                    "parentType": null,
+                    "timeCreate": "2017-04-27T17:27:02.313Z",
+                    "timeModify": "2017-04-27T17:27:02.313Z",
+                    "userCan": [
+                        "EDIT_ENTITY",
+                        "MANAGE_ENTITY",
+                        "VIEW_ENTITY"
+                    ],
+                    "userRole": "owner"
+                },
+                "1493340729194": {
+                    "roleFor": "project",
+                    "userId": "910200869",
+                    "entityId": "1493340729194",
+                    "createdById": null,
+                    "entityName": "role",
+                    "entityStatus": "1",
+                    "id": "-585039974",
+                    "modifiedById": null,
+                    "parentId": null,
+                    "parentType": null,
+                    "timeCreate": "2017-04-28T00:52:09.193Z",
+                    "timeModify": "2017-04-28T00:52:09.193Z",
+                    "userCan": [
+                        "EDIT_ENTITY",
+                        "MANAGE_BIDDING_CATEGORY_PROJECT",
+                        "MANAGE_ENTITY",
+                        "VIEW_BIDDING_CATEGORY_PROJECT",
+                        "VIEW_ENTITY"
+                    ],
+                    "userRole": "owner"
+                },
+                "project": [
+                    "1493340729194"
+                ],
+                "account": [
+                    "910200869"
+                ],
+                "contractor": []
+            }
+        },
+        {
+            "token": "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTEwMjAwODY5LCJrZXlKV1QiOjE1MDA2NTg4MDg4NzYsImlhdCI6MTUwMDY1ODgwOH0.P4gWncnSh1kQnoooDPhQDrYi3NPtPXpBpUiLCD8HJ4k"
+        }
+    ]
+}
+var responseLoginFail = {
+    "error": {
+        "ec": "LOGIN_FAIL",
+        "ms": "Đăng nhập lỗi"
+    }
+}
