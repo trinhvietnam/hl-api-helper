5b7997f98fd0d5d9780ea5ed755785565fa22d6a
DESKTOP-8CA0JAG\nam <trinhvietnambk@gmail.com>
2017-07-22 16:53:41 +0700

    add responseProjectOfAccount

--- response-sample/accounts.ts
+++ response-sample/accounts.ts
@@ -16,3 +16,77 @@ var responseUpdateAccountInfoOK ={
         }
     ]
 }
+var responseProjectOfAccount = {
+    "error": null,
+    "data": [
+        {
+            "id": 1493340729194,
+            "entityStatus": 1,
+            "timeCreate": "2017-04-28T00:52:09.228Z",
+            "timeModify": "2017-05-24T16:55:45.786Z",
+            "entityName": "project",
+            "numOfFollower": "2",
+            "numOfView": 0,
+            "numOfRating": 0,
+            "numOfRating1Star": 0,
+            "numOfRating2Star": 0,
+            "numOfRating3Star": 0,
+            "numOfRating4Star": 0,
+            "numOfRating5Star": 0,
+            "isReadyForBidding": true,
+            "typeOfContractor": [
+                "1"
+            ],
+            "typeOfContruction": 2,
+            "typeOfProject": 2,
+            "name": "Project edited",
+            "country": "Việt Nam",
+            "city": "Bà Rịa-Vũng Tàu",
+            "district": "Châu Đức",
+            "location": {
+                "lat": 21.0309471,
+                "lon": 105.781722
+            },
+            "address": "Số 4 - Ngõ 191 -Phố Khương Thượng",
+            "phase": 1,
+            "fromBudget": 3,
+            "endBudget": 33252,
+            "unitBugget": "VND",
+            "typeOfInvestment": 1,
+            "startBuildTime": "2017-04-29T00:00:00.000Z",
+            "endBuildTime": "2017-06-30T00:00:00.000Z",
+            "startScheduled": "2017-05-03T00:00:00.000Z",
+            "endScheduled": "2017-06-03T00:00:00.000Z",
+            "bidSolicitor": [
+                123456789
+            ],
+            "biddingForm": 1,
+            "countryOfContractor": "Việt Nam",
+            "districtOfContractor": "Bà Rịa-Vũng Tàu",
+            "bidOpenTime": "2017-04-28T00:00:00.000Z",
+            "bidCloseTime": "2017-05-06T00:00:00.000Z",
+            "minimumExperienceOfContractor": 5,
+            "creditRatingOfContractor": 4,
+            "minimumRevenueOfContractor": 10000,
+            "unitRevenueOfContractor": "VND",
+            "ownerId": "910200869",
+            "createdById": null,
+            "noSignName": "project edited",
+            "modifiedById": 910200869,
+            "documents": [
+                {
+                    "timeCreate": "02/05/2017 23:04",
+                    "name": "Tài liệu thiết kế",
+                    "url": "https://houselink.com.vn/files/1232",
+                    "fileId": "1232"
+                },
+                {
+                    "timeCreate": "02/05/2017 23:10",
+                    "name": "Tài liệu kết cấu",
+                    "url": "https://houselink.com.vn/files/2332",
+                    "fileId": "2332"
+                }
+            ]
+        }
+    ]
+}
