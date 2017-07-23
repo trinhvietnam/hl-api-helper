038d08de9748d0c5e3f4bd718e8f4116a1def353
DESKTOP-8CA0JAG\nam <trinhvietnambk@gmail.com>
2017-07-23 16:13:30 +0700

    add create contractor response

---
+++ response-sample/contractors.ts
@@ -0,0 +1,150 @@
+var responseCreateContractors = {
+    "error": null,
+    "data": [
+        {
+            "id": 1500801173132,
+            "entityStatus": 1,
+            "numOfFollower": 0,
+            "numOfView": 0,
+            "numOfRating": 0,
+            "numOfRating1Star": 0,
+            "numOfRating2Star": 0,
+            "numOfRating3Star": 0,
+            "numOfRating4Star": 0,
+            "numOfRating5Star": 0,
+            "unitRevenue": "VND",
+            "unitPrice": "VND",
+            "name": "Tong cong ty Vinaconex",
+            "city": "Hà Nội",
+            "country": "Việt Nam",
+            "address": "Phạm Hùng - Hà Nội",
+            "phone": "0934523 32420",
+            "email": "vi@gmail.com",
+            "typeOfContractor": [
+                "1.1",
+                "1.2"
+            ],
+            "district": "Cầu Giấy",
+            "origination": "Việt Nam",
+            "foundedYear": 1960,
+            "typeCompany": 1,
+            "revenue": 123454535,
+            "ownerId": 910200869,
+            "createdById": 910200869,
+            "userEntity": {
+                "id": 910200869,
+                "accountStatus": null,
+                "accountType": null,
+                "address": null,
+                "avatar": null,
+                "city": "TP Hồ Chí Minh",
+                "country": "Việt Nam",
+                "createdById": null,
+                "district": null,
+                "email": "trinhvietnambk@gmail.com",
+                "emailNotifies": null,
+                "entityName": "account",
+                "entityStatus": "1",
+                "fax": null,
+                "isValidatePhone": null,
+                "isWaitingResetPassword": null,
+                "language": null,
+                "lastName": null,
+                "location": null,
+                "modifiedById": "910200869",
+                "name": "Trịnh Viết Nam",
+                "numOfFollower": null,
+                "numOfFollowing": "2",
+                "numOfRating": null,
+                "numOfRating1Star": null,
+                "numOfRating2Star": null,
+                "numOfRating3Star": null,
+                "numOfRating4Star": null,
+                "numOfRating5Star": null,
+                "numOfView": null,
+                "parentId": null,
+                "parentType": null,
+                "paymentMethods": null,
+                "phone": null,
+                "suggestEmail": "trinhvietnambk",
+                "taxCode": null,
+                "tempPassword": null,
+                "timeCreate": "2017-04-27T17:27:02.330Z",
+                "timeModify": "2017-07-22T09:50:26.595Z",
+                "type": null,
+                "verifies": null,
+                "website": null,
+                "keyJWT": 1500799400945,
+                "mapRoles": {
+                    "910200869": {
+                        "roleFor": "account",
+                        "userId": "910200869",
+                        "entityId": "910200869",
+                        "createdById": null,
+                        "entityName": "role",
+                        "entityStatus": "1",
+                        "id": "1105731116",
+                        "modifiedById": null,
+                        "parentId": null,
+                        "parentType": null,
+                        "timeCreate": "2017-04-27T17:27:02.313Z",
+                        "timeModify": "2017-04-27T17:27:02.313Z",
+                        "userCan": [
+                            "EDIT_ENTITY",
+                            "MANAGE_ENTITY",
+                            "VIEW_ENTITY"
+                        ],
+                        "userRole": "owner"
+                    },
+                    "contractor": [],
+                    "account": [
+                        "910200869"
+                    ],
+                    "1493340729194": {
+                        "roleFor": "project",
+                        "userId": "910200869",
+                        "entityId": "1493340729194",
+                        "createdById": null,
+                        "entityName": "role",
+                        "entityStatus": "1",
+                        "id": "-585039974",
+                        "modifiedById": null,
+                        "parentId": null,
+                        "parentType": null,
+                        "timeCreate": "2017-04-28T00:52:09.193Z",
+                        "timeModify": "2017-04-28T00:52:09.193Z",
+                        "userCan": [
+                            "EDIT_ENTITY",
+                            "MANAGE_BIDDING_CATEGORY_PROJECT",
+                            "MANAGE_ENTITY",
+                            "VIEW_BIDDING_CATEGORY_PROJECT",
+                            "VIEW_ENTITY"
+                        ],
+                        "userRole": "owner"
+                    },
+                    "project": [
+                        "1493340729194"
+                    ]
+                }
+            },
+            "noSignName": "tong cong ty vinaconex"
+        },
+        {
+            "entityId": 1500801173132,
+            "userId": 910200869,
+            "userRole": "manager",
+            "entityName": "role",
+            "userCan": [
+                "MANAGE_ENTITY",
+                "EDIT_ENTITY",
+                "VIEW_ENTITY",
+                "RATING_OTHER_ENTITY",
+                "SEND_BIDDING_CATEGORY_PROJECT",
+                "EDIT_BIDDING_CATEGORY_PROJECT",
+                "VIEW_BIDDING_CATEGORY_PROJECT"
+            ],
+            "roleFor": "role",
+            "id": -300338983
+        }
+    ]
+}
