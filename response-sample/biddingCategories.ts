487b11cbbc59c25eaaa3062914969d15e824826b
DESKTOP-8CA0JAG\nam <trinhvietnambk@gmail.com>
2017-07-24 23:58:17 +0700

    add response create biddingCategory

---
+++ response-sample/biddingCategories.ts
@@ -0,0 +1,31 @@
+var responseCreateBiddingCategoryOk = {
+    "error": null,
+    "data": [
+        {
+            "id": 1500915220804,
+            "projectId": 1493340729194,
+            "createById": 1493340729194,
+            "createByType": "project",
+            "typeOfContractor": [
+                "1.1",
+                "1.1.2"
+            ],
+            "name": "Gói thầu thiết kế",
+            "noSignName": "goi thau thiet ke",
+            "createByUserId": 910200869
+        },
+        {
+            "entityName": "role",
+            "entityId": 1500915220804,
+            "userId": 1493340729194,
+            "userRole": "manager",
+            "userCan": [
+                "MANAGE_ENTITY",
+                "EDIT_ENTITY",
+                "VIEW_ENTITY"
+            ],
+            "roleFor": "biddingCategory",
+            "id": 1873388219
+        }
+    ]
+}
